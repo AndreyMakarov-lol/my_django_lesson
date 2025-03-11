cb0cd937e89abae7f326b03539d10575bf123e7f
Andrey_Makarov <64976956+AndreyMakarov-lol@users.noreply.github.com>
2025-03-11 09:31:44 +0300

    test1

--- my_project/settings.py
+++ my_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'my_first_app',
     'orders',
+    'products',
 ]
 
 MIDDLEWARE = [
--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from products.models import Product
+
 
 # Create your models here.
 class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    #on_delete=
+#CASCADE каскадное удаление(удалится пользователь значит удалаятся все его заказы,
+#SET_NULL если удалится пользователь то у заказа поле пользователь будет =0
+#PROTECT не позволит удалять пользователя пока у него есть заказы
+    product = models.ManyToManyField(Product)
