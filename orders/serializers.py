f7c9425c90ee2f9a049a6fcc8c1322a1d3e0666e
Andrey_Makarov <64976956+AndreyMakarov-lol@users.noreply.github.com>
2025-03-18 17:12:30 +0300

    add table on page orders page

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
-
+from django.contrib.auth.models import User
 from orders.models import SalesOrder
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']  # Убедитесь, что username включен
 
 
 class OrderSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)  # Сериализация связанного пользователя
     class Meta:
         model = SalesOrder
-        fields = ['amount', 'description']
+        fields = ['amount', 'description', 'user', 'product']
